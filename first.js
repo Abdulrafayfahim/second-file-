4f897891932fba9503c07194c00846795aeb94f3
Abdulrafayfahim <rafayirfan586@gmail.com>
2024-08-16 16:53:43 +0500

    first commit

---
+++ first.js
@@ -0,0 +1,26 @@
+var a=45;
+var b=28;
+var c=a*b;
+console.log(c);
+document.write(c);
+
+
+// alert("Enter your name");
+// prompt("enter your name");
+
+
+
+var a =28;
+if(a>=34){
+    console.log("vote cast");
+
+}
+else{
+    console.log("does not vote cast");
+}
+
+
+
+for(a=0;a<=1000;a++){
+    console.log(a);
+}
